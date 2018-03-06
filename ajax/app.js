077035660c6e65ffaf2c4c06a4585f8a3efa7e6e
Ben Lee <1019229581@qq.com>
2018-03-06 21:32:00 +0800

    ajax.data---req.body

--- ajax/app.js
+++ ajax/app.js
@@ -22,9 +22,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    
+    // res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("X-Powered-By",'Express/4.15.2');
+    next();
+});
+
+
+app.use(['/','/\*.html'], index);
+app.use('/users', users);
+// app.use('/xhr.html', index);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- ajax/routes/index.js
+++ ajax/routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/',function(req, res, next){
+	res.send('ok post,right away');
+});
+
+// some html pages
+var options = {
+	root: process.cwd() + '/pages/'
+}
+
+router.get('/xhr.html',function(req, res, next){
+	res.sendFile('./xhr.html', options, function(err){
+		if(err) {
+			console.log(err);
+			res.status(err.status).end();
+		}
+		/*else 
+			console.log("Sent: ")*/
+	});
+});
+
+router.post('/xhr.html',function(req, res, next){
+	console.log(req.body)
+	res.send('ok post right away');
+
+});
+
+router.get('/hhh.html', function(req, res, next){
+	res.send('Yeah, you, hhh');
+});
 module.exports = router;
